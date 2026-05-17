67e2221ea52ea39b9e559958c591d7a9075f2139
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-17 22:38:27 +0100

    Añadí los métodos con los requisitos que me pedía la actividad

--- src/net/salesianoslacuesta/gestor/Gestor.java
+++ src/net/salesianoslacuesta/gestor/Gestor.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import net.salesianoslacuesta.videojuegos.Videojuegos;
 
 public class Gestor {
-    private ArrayList<Videojuego> listaVideojuegos;
+    private ArrayList<Videojuegos> listaVideojuegos;
     private int totalAñadidos;
 
     public Gestor() {
@@ -29,6 +29,57 @@ public class Gestor {
 
     public void setTotalAñadidos(int totalAñadidos) {
         this.totalAñadidos = totalAñadidos;
+    }
+
+    public void añadirVideojuego( Videojuego juego) {
+        listaVideojuegos.add(juego);
+        totalAñadidos++;
+    }
+
+    public void listarVideojuego() {
+        if (listaVideojuegos.isEmpty()) {
+            System.out.println("No hay videojuegos en la lista");
+        } else {
+            for (int i = 0; i < listaVideojuegos.size(); i++) {
+                System.out.println(listaVideojuegos.get(i));
+            }
+        }
+    }
+
+    public Videojuego buscarPorNombre(String nombre) {
+       for (int i = 0; i < listaVideojuegos.size(); i++) {
+        if (listaVideojuegos.get(i).getNombre().equals(nombre)) {
+          return listaVideojuegos.get(i);  
+        }
+            
+       }
+       return null;
+    }
+
+    public boolean eliminarVideojuego(String nombre) {
+        for (int i = 0; i < listaVideojuegos.size(); i++) {
+            if (listaVideojuegos.get(i).getNombre().equals(nombre)) {
+                listaVideojuegos.remove(i);
+                totalAñadidos--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean editarVideojuego(String nombre, String nuevoNombre, String nuevoGenero, String nuevaPlataforma, int nuevaPuntuacion) {
+        for (int i = 0; i < listaVideojuegos.size() ; i++) {
+            Videojuegos juego = listaVideojuegos.get(i);
+
+            if(juego.getNombre().equals(nombre)) {
+                juego.setNombre(nuevoNombre);
+                juego.setGenero(nuevoGenero);
+                juego.setPlataforma(nuevaPlataforma);
+                juego.setPuntuacion(nuevaPuntuacion);
+                return true;
+            }
+        }
+        return false;
     }
 }
 
