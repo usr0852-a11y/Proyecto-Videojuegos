4be033e321cbcce9ddb925daf7a98aec0677ead7
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-17 22:07:03 +0100

    Creé la clase gestor y le añadí una ArrayList además de otro atributo de total de juegos añadidos

---
+++ src/net/salesianoslacuesta/gestor/Gestor.java
@@ -0,0 +1,16 @@
+package net.salesianoslacuesta.gestor;
+
+import java.util.ArrayList;
+
+public class Gestor {
+    private ArrayList<Videojuego> listaVideojuegos;
+    private int totalAñadidos;
+
+    public Gestor() {
+        listaVideojuegos = new ArrayList<>();
+        totalAñadidos = 0;
+    }
+}
+
+//CREAR RAMA EN GIT BASH
+
