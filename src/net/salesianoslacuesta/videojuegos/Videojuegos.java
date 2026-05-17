432ce554eaea80e61fb6c5af522df16232281028
Samuel Afonso <usr0852@salesianos-lacuesta.net>
2026-05-17 21:09:53 +0100

    Añadí getters y setters y además modifiqué la puntuación en el constructor para simplificarlo ya que lo tenía mal

--- src/net/salesianoslacuesta/videojuegos/Videojuegos.java
+++ src/net/salesianoslacuesta/videojuegos/Videojuegos.java
@@ -10,12 +10,45 @@ public class Videojuegos {
         this.nombre = nombre;
         this.genero = genero;
         this.plataforma = plataforma;
-        
-        if (puntuacion >= 1 && puntuacion <= 5) {
+        this.puntuacion = puntuacion;
+
+    }
+
+    // Getters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+         if (puntuacion >= 1 && puntuacion <= 5) {
             this.puntuacion = puntuacion;
         } else {
             this.puntuacion = 1;
         }
-
     }
 }
